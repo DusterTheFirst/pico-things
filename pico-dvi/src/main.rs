eb01a7e648b15ec233ac34f02aa141c2148c8578
Zachary Kohnen <me@dusterthefirst.com>
2023-03-18 20:54:17 +0100

    overclock

--- pico-dvi/src/main.rs
+++ pico-dvi/src/main.rs
@@ -6,19 +6,18 @@ use embedded_hal::digital::v2::OutputPin;
 use panic_probe as _;
 
 // Board support package
-use rp_pico as bsp;
+use rp_pico::{self as bsp};
 
 use bsp::{
-    hal::{
-        clocks::{self, init_clocks_and_plls},
-        gpio::PinState,
-        watchdog::Watchdog,
-        Clock, self,
-    },
+    hal::{gpio::PinState, sio::Sio, watchdog::Watchdog, Clock},
     pac, Pins,
 };
 use cortex_m::delay::Delay;
-use defmt::info;
+use defmt::{dbg, info};
+
+use crate::clock::init_clocks;
+
+mod clock;
 
 // Separate macro annotated function to make rust-analyzer fixes apply better
 #[bsp::entry]
@@ -33,24 +32,18 @@ fn entry() -> ! {
     let core_peripherals = pac::CorePeripherals::take().unwrap();
 
     let mut watchdog = Watchdog::new(peripherals.WATCHDOG);
-    let single_cycle_io = hal::sio::Sio::new(peripherals.SIO);
+    let single_cycle_io = Sio::new(peripherals.SIO);
 
     // External high-speed crystal on the pico board is 12Mhz
-    const EXTERNAL_CRYSTAL_OSCILLATOR_FREQ_HZ: u32 = 12_000_000;
-    let clocks = match init_clocks_and_plls(
-        EXTERNAL_CRYSTAL_OSCILLATOR_FREQ_HZ,
+    let mut clocks = init_clocks(
         peripherals.XOSC,
+        peripherals.ROSC,
         peripherals.CLOCKS,
         peripherals.PLL_SYS,
         peripherals.PLL_USB,
         &mut peripherals.RESETS,
         &mut watchdog,
-    ) {
-        Ok(clocks) => clocks,
-        Err(clocks::InitError::ClockError(error)) => panic!("{:?}", error),
-        Err(clocks::InitError::PllError(error)) => panic!("{:?}", error),
-        Err(clocks::InitError::XoscErr(error)) => panic!("{:?}", error),
-    };
+    );
 
     let pins = Pins::new(
         peripherals.IO_BANK0,
@@ -59,27 +52,30 @@ fn entry() -> ! {
         &mut peripherals.RESETS,
     );
 
-    let mut led_pin = pins.gpio0.into_push_pull_output_in_state(PinState::Low);
+    let mut led_pin = pins.gpio15.into_push_pull_output_in_state(PinState::Low);
 
-    let mut delay = Delay::new(core_peripherals.SYST, clocks.system_clock.freq().to_Hz());
+    let mut delay = Delay::new(
+        core_peripherals.SYST,
+        dbg!(clocks.system_clock.freq().to_Hz()),
+    );
 
     loop {
         info!("high");
         led_pin.set_high().unwrap();
         delay.delay_ms(500);
-        info!("off");
+        info!("low");
         led_pin.set_low().unwrap();
         delay.delay_ms(500);
     }
 }
 
-struct DviPHY {
-
-}
+struct DviPHY {}
 
 impl DviPHY {
     pub fn new() {
-        pio_proc::pio_file!("src/test.pio");
-        // pio::Assembler::new().
+        let program_a = pio_proc::pio_file!("src/test.pio", select_program("a"));
+        let program_b = pio_proc::pio_file!("src/test.pio", select_program("b"));
+
+        let mut program_c = pio::Assembler::<{ pio::RP2040_MAX_PROGRAM_SIZE }>::new();
     }
 }
